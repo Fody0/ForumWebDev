195d5864df5eaa1f02961819c88ce09cb34e2ca6
artyomnoskov15 <artyomnoskov15@gmail.com>
2024-09-12 10:43:34 +0300

    Backend upgrade

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,18 @@
+import "dotenv/config";
+import env from "../src/util/validateEnv";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = undefined;
 
 app.get("/", (req: express.Request, res: express.Response) => {
-    res.send("Hello, World!");
-});
+    res.send("Hello World!");
+})
+const port = env.PORT;
 
-app.listen(port!, () => {
-    console.log(`Server started on port ${port}`);
-});
+
+mongoose.connect(env.MONGODB_CONNECTION_STRING).then(() => {
+    console.log("MongoDB Connected!");
+    app.listen(port, () => {
+        console.log(`Server started on port ${port}`);
+    })
+})
